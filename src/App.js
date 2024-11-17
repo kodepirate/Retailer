50e4b2c6ed1ae80a8439d42ea8db3f458caeb9ea
diptajit <sahadiptajit.2020@gmail.com>
2024-11-17 19:31:45 +0530

    add docker compose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='bg-slate-500 h-screen text-center flex flex-col justify-center font-bold'>
-      hello world
+      hello world Hii
     </div>
   );
 }
